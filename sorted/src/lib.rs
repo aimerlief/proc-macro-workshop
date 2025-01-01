424fe77e0e6613298f64bb5f106b462ca84aaeca
aimerlief <152078880+aimerlief@users.noreply.github.com>
2025-01-01 16:38:17 +0900

    sorted: 02 not enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::ToTokens;
 use syn::parse_macro_input;
 
@@ -6,5 +7,21 @@ use syn::parse_macro_input;
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let item = parse_macro_input!(input as syn::Item);
-    item.to_token_stream().into()
+    
+    match validate_enum(&item) {
+        // Same as proc_macro::TokenStream::from(i.to_token_stream())
+        Ok(i) => i.to_token_stream().into(),
+        Err(e) => e.to_compile_error().into(), 
+    }
+}
+
+fn validate_enum(item: &syn::Item) -> Result<&syn::ItemEnum, syn::Error> {
+    if let syn::Item::Enum(item_enum) = item {
+        Ok(item_enum)
+    } else {
+        Err(syn::Error::new(
+            Span::call_site(),
+            "expected enum or match expression",
+        ))
+    }
 }
