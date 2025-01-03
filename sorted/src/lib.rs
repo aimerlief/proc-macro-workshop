b4800c2e9a68be3260c4cb11bdf676f8a59c98d0
aimerlief <152078880+aimerlief@users.noreply.github.com>
2025-01-04 06:21:56 +0900

    sorted: 04 variants with data

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,21 +1,21 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use quote::ToTokens;
 use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut output = input.clone();
     let _ = args;
     let item = parse_macro_input!(input as syn::Item);
     
-    match validate_enum(item) {
-        // Same as proc_macro::TokenStream::from(i.to_token_stream())
-        Ok(i) => i.to_token_stream().into(),
-        Err(e) => e.to_compile_error().into(), 
+    if let Err(e) = validate_enum(item) {
+        output.extend(TokenStream::from(e.to_compile_error()));
     }
+
+    output
 }
 
-fn validate_enum(item: syn::Item) -> Result<syn::ItemEnum, syn::Error> {
+fn validate_enum(item: syn::Item) -> Result<(), syn::Error> {
     if let syn::Item::Enum(item_enum) = item {
         let variants = &item_enum.variants;
 
@@ -25,18 +25,20 @@ fn validate_enum(item: syn::Item) -> Result<syn::ItemEnum, syn::Error> {
                 let earlier = &variants[j];
         
                 if current.ident.to_string() < earlier.ident.to_string() {
-                    return Err(syn::Error::new(
-                        current.ident.span(),
-                        format!(
-                            "{} should sort before {}",
-                            current.ident, earlier.ident
-                        ),
-                    ));
+                    return Err(
+                        syn::Error::new(
+                            current.ident.span(),
+                            format!(
+                                "{} should sort before {}",
+                                current.ident, earlier.ident
+                            )
+                        )
+                    );
                 }
             }
         }
 
-        Ok(item_enum)
+        Ok(())
     } else {
         Err(syn::Error::new(
             Span::call_site(),
