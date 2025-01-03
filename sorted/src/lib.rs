41404075a0066108ff3e91571c29411489c4d55c
aimerlief <152078880+aimerlief@users.noreply.github.com>
2025-01-04 03:10:10 +0900

    sorted: 03 out of order

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -8,15 +8,34 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let item = parse_macro_input!(input as syn::Item);
     
-    match validate_enum(&item) {
+    match validate_enum(item) {
         // Same as proc_macro::TokenStream::from(i.to_token_stream())
         Ok(i) => i.to_token_stream().into(),
         Err(e) => e.to_compile_error().into(), 
     }
 }
 
-fn validate_enum(item: &syn::Item) -> Result<&syn::ItemEnum, syn::Error> {
+fn validate_enum(item: syn::Item) -> Result<syn::ItemEnum, syn::Error> {
     if let syn::Item::Enum(item_enum) = item {
+        let variants = &item_enum.variants;
+
+        for i in 0..variants.len() {
+            let current = &variants[i];
+            for j in 0..i {
+                let earlier = &variants[j];
+        
+                if current.ident.to_string() < earlier.ident.to_string() {
+                    return Err(syn::Error::new(
+                        current.ident.span(),
+                        format!(
+                            "{} should sort before {}",
+                            current.ident, earlier.ident
+                        ),
+                    ));
+                }
+            }
+        }
+
         Ok(item_enum)
     } else {
         Err(syn::Error::new(
